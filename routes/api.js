dd5a427d98ecb3a57d7d9127f80a205f632a2e60
Matthew Merrill <MattMerr47@gmail.com>
2018-02-19 18:56:27 -0800

    Post launch scripts, fancy stuff, yeah.

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,10 @@ const child_process = require('child_process');
 const path = require('path');
 
 let project_procs = {};
-let project_ports = {};
+let project_numberings = {};
 let listeners = new Set();
 
-let portsUsed = {};
+let projectNumbersUsed = {};
 
 module.exports = function(io, nconf) {
 
@@ -20,14 +20,23 @@ module.exports = function(io, nconf) {
   const auth_db = low(new FileSync(nconf.get('config:auth')));
 
   db._.mixin(lodashId);
-  db.defaults({ projects: [], urlTemplate: 'http://www.mattmerr.com' })
+  db.defaults({
+      projects: [],
+      urlTemplate: 'http://www.mattmerr.com/#CONFIGURE_C9DASH_$PORT',
+      baseProjectNumber: 0,
+      basePort: 4200,
+      postLaunch: ['echo "Server $PORT Launched!"'],
+    })
     .write();
 
   io.on('connection', function(socket) {
     listeners.add(socket);
     socket.on('disconnect', () => listeners.delete(socket));
-    for (let id in project_ports) {
-      socket.emit('started', [id, db.get('urlTemplate').value().replace('$PORT', project_ports[id])]);
+    for (let id in project_numberings) {
+      let url = db.get('urlTemplate')
+                  .value()
+                  .replace('$PORT', ('00' + project_numberings[id]).slice(-2));
+      socket.emit('started', [id, url]);
     }
   });
 
@@ -47,24 +56,43 @@ module.exports = function(io, nconf) {
       .getById(req.body.id)
       .value();
 
-    if (project && (!project_procs[project.id] || project_procs[project.id].killed)) {
-      let port = 8100;
-      while (portsUsed[port]) {
-        port += 1;
+    if (project && (!project_procs[project.id])) {
+      let projectNumber = db.get('baseProjectNumber').value();
+      while (projectNumbersUsed[projectNumber]) {
+        projectNumber += 1;
       }
+      let port = projectNumber + db.get('basePort').value();
 
-      project_procs[project.id] = child_process.fork(
+      project_procs[project.id] = [child_process.fork(
         '/opt/c9sdk/server.js',
         [
           '-w', path.join('/home/merrillm/', project.directory),
           '-p', port,
           '--auth', 
             auth_db.get('username') + ':' + auth_db.get('password').value()
-        ]);
-      project_ports[project.id] = port;
-      portsUsed[port] = true;
+        ])];
+      
+      
+      let postLaunch = db.get('postLaunch').value()
+      if (!Array.isArray(postLaunch)) {
+        postLaunch = [postLaunch]
+      }
+      
+      for (let idx = 0; idx < postLaunch.length; idx += 1) {
+        let postLaunchScript = postLaunch[idx].replace('$PORT', port)
+        let child = child_process.exec(postLaunchScript);
+        child.stdout.on('data', function(data) {
+          console.log('p' + projectNumber + '[' + idx +']: ', data.trimRight()); 
+        });
+        project_procs[project.id].push();
+      }
+      
+      project_numberings[project.id] = projectNumber;
+      projectNumbersUsed[projectNumber] = true;
 
-      let url = db.get('urlTemplate').value().replace('$PORT', port);
+      let url = db.get('urlTemplate')
+                  .value()
+                  .replace('$PORT', ('00' + projectNumber).slice(-2));
 
       listeners.forEach(socket =>
         socket.emit('started', [project.id, url]));
@@ -81,10 +109,12 @@ module.exports = function(io, nconf) {
       .value();
 
     if (project && project_procs[project.id] && !project_procs[project.id].killed) {
-      project_procs[project.id].kill();
-      project_procs[project.id] = undefined;
-      portsUsed[project_ports[project.id]] = undefined;
-      project_ports[project.id] = undefined;
+      for (let proc of project_procs[project.id]) {
+        proc.kill();
+      }
+      delete project_procs[project.id];
+      delete projectNumbersUsed[project_numberings[project.id]];
+      delete project_numberings[project.id];
 
       listeners.forEach(socket => socket.emit('stopped', [project.id]));
       res.sendStatus(200);
@@ -116,3 +146,5 @@ module.exports = function(io, nconf) {
 
   return router;
 };
+
+
